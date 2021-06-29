9ec91263214b122e97607e9f8eccd269f0347aef
singcl <1276837689@qq.com>
2021-06-29 17:51:18 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "正在压缩, 请稍后..."
+
+tar -zcvf react-hooks-todolist.tar.gz ./server ./README.md ./deploy.sh
+
+echo "上传中..."
+
+sshpass -p ${password} scp ./react-hooks-todolist.tar.gz  ${user}@${host}:/home/quzhanxiang/react-hooks-todolist/
+sshpass -p ${password} ssh ${user}@${host} "cd /home/quzhanxiang/react-hooks-todolist/ && tar -xzvf react-hooks-todolist.tar.gz && bash build_production.sh"
